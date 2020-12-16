946a0856619684551ce5907af6edfd68a4b208b3
YoshuaKN <yoshuakennynugroho@gmail.com>
2020-12-16 09:46:01 +0700

    change user.roles to list

---
+++ crud/src/main/java/com/mcc40/crud/services/MyUserDetailsService.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mcc40.crud.services;
+
+import com.mcc40.crud.entities.MyUserDetails;
+import com.mcc40.crud.entities.User;
+import com.mcc40.crud.repositories.UserRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Yoshua
+ */
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(userName);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + userName));
+
+        return user.map(MyUserDetails::new).get();
+    }
+}
