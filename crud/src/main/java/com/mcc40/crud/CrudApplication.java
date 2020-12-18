179de4a111410b8b43b537ac370fb24f5cc850e5
YoshuaKN <yoshuakennynugroho@gmail.com>
2020-12-18 16:24:27 +0700

    add Swagger

--- crud/src/main/java/com/mcc40/crud/CrudApplication.java
+++ crud/src/main/java/com/mcc40/crud/CrudApplication.java
@@ -2,35 +2,16 @@ package com.mcc40.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CrudApplication {
 
     public static void main(String[] args) {
-//        EntityManagerFactory emf
-//                = Persistence.createEntityManagerFactory("example-unit");
-//        try {
-//            persistEntity(emf);
-//        } finally {
-//            emf.close();
-//        }
         SpringApplication.run(CrudApplication.class, args);
         System.out.println("Sudah berjalan, ga usah nungguin lagi!!!");
     }
 
-//    private static void persistEntity(EntityManagerFactory emf) {
-//        EntityManager em = emf.createEntityManager();
-//        em.setFlushMode(FlushModeType.COMMIT);
-//        List<Employee> employeeList = getNewEmployees();
-//
-//        em.getTransaction().begin();
-//        for (Employee employee : employeeList) {
-//            em.persist(employee);
-//        }
-//        em.flush();
-//        showPersistedITEmployees(em);
-//        em.getTransaction().commit();
-//        em.close();
-//    }
 
 }
